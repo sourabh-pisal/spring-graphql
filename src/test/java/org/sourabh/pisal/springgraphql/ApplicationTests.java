d16825f3ded57da2053f33d4f34d1ad67bea39fa
Sourabh Pisal <pisal.sourabh@gmail.com>
2019-12-01 19:43:49 +0200

    Basic Spring boot application

---
+++ src/test/java/org/sourabh/pisal/springgraphql/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sourabh.pisal.springgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
